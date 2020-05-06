f5d2e393ece884fd911aacb8e7237e7fe33ef1b8
AdrianaMeireles80 <adriana_1998@live.com.pt>
2020-05-06 23:35:05 +0100

    cliente/servidor

--- Origin.java
+++ Origin.java
@@ -1,21 +1,50 @@
-
-
 // cliente comunica com o AnonGW
 //AnonGW comunica com o servidor
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Origin{
+	
 
+  public static void main(String[] args) throws Exception{
+		System.out.println("Cliente iniciado.");
 
-  public static void main(String[] args) throws IOException{
-    Socket socket = new Socket("10.3.3.1", 80);
-    DataInputStream in = new DataInputStream(socket.getInputStream());
-    System.out.println(in.readUTF());
-    DataOutputStream out = new DataOutputStream((socket.getOutputStream()));
-    out.writeUTF("waiting for connection");
-    socket.close();
-  }
+		System.out.println("Iniciar conexão com o servidor");
 
+		Socket socket = new Socket("10.3.3.1",80);
+
+		System.out.println("Conexão Estabelecida.");
+
+		InputStream input = socket.getInputStream();
+
+		OutputStream output = socket.getOutputStream();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(input));
+		PrintStream out = new PrintStream(output);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		while(true){
+
+			System.out.print("Escreva uma mensagem: ");
+			
+			String mensagem = scan.nextLine();				
+
+			out.println(mensagem);
+			if("FIM".equals(mensagem)){
+				break;
+			}
+			mensagem = in.readLine();
+			System.out.println(
+				"Mensagem recebida do servidor: " +
+				mensagem);
+		}
+		System.out.println("Conexão encerrada.");
+		in.close();
+		out.close();
+		socket.close();
+   }
 }
+ 
--- TargetServer.java
+++ TargetServer.java
@@ -1,19 +1,46 @@
-//servidor
-
 import java.net.*;
 import java.io.*;
 
-public class TargetServer{
 
+public class TargetServer {
+  
+  public static void main(String[] args) throws Exception {
+    
+      System.out.println("Servidor iniciado.");
+      // Instancia o ServerSocket ouvindo a porta 80
+      ServerSocket servidor = new ServerSocket(80);
+      System.out.println("Aguardar Conexão de um cliente.");
+      
+        // o método accept() bloqueia a execução até que
+        // o servidor receba um pedido de conexão
+        Socket socket = servidor.accept();
 
- public static void main(String[] args) throws IOException{
-  ServerSocket servsocket = new ServerSocket(80);
-  Socket socket = servsocket.accept();
-  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-  out.writeUTF("weleleleleele");
-  DataInputStream in = new DataInputStream(socket.getInputStream());
-  System.out.println(in.readUTF());
-  socket.close();
- }
+        System.out.println("Conexão estabelecida");
+        
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream();
 
+        BufferedReader in = new BufferedReader(new InputStreamReader(input));
+        PrintStream out = new PrintStream(output);
+        
+        while(true){
+		String mensagem = in.readLine();
+ 		
+		System.out.println("Mensagem recebida do cliente [" +
+                socket.getInetAddress().getHostName()+
+                 "]: " + 
+                mensagem);
+		
+		if("FIM".equals(mensagem)){
+			break;
+         }
+                out.println(mensagem);
+        }
+	System.out.println("Conexão encerrada.");
+	in.close();
+	out.close();
+	socket.close();
+	System.out.println("Servidor encerrado");
+        servidor.close();       
+   }
 }
